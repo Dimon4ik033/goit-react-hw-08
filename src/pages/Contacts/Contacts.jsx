6302b3edd1a03e4febd640e819742b95747c151e
Dmitry Naumenko <dimanaumenko31@gmail.com>
2025-02-04 20:39:22 +0200

    fix-useEffect

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,13 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ContactForm from '../../components/ContactForm/ContactForm';
 import { ContactList } from '../../components/ContactList/ContactList';
 import SearchBox from '../../components/SearchBox/SearchBox';
 import { selectError, selectIsLoading } from '../../redux/contacts/selectors';
 import { ProgressBar } from 'react-loader-spinner';
+import { useEffect } from 'react';
+import { fetchContacts } from '../../redux/contacts/operations';
 
 export default function ContactPage() {
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <div className='flex flex-col justify-center items-center mt-15'>
